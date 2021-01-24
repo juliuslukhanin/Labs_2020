a25c31f9d75463e20d2a96472e02c92290ba6a68
julius lukhanin <julius.lukhanin@outlook.com>
2021-01-24 19:57:09 +0200

    commit  - add  lab02_05

--- lab02_05/task05.cpp
+++ lab02_05/task05.cpp
@@ -6,8 +6,26 @@ using namespace std;
 
 int main() {
 
-	
+	double varX = {};
+	double epsilon = {};
 
+	while (true)
+	{
+		cout << "Input x = ";
+		cin >> varX;
+		cout << "Input epsilon = ";
+		cin >> epsilon;
+		double varA = 1;
+		double sum = varA;
+		int index = 0;
+		do
+		{
+			index++;
+			varA *= varX / index;
+			sum += varA;
+		} while (abs(varA) > epsilon);
+		cout << " epsilon = " << epsilon << "    sum =" << setprecision(12) << sum << endl;
+	}
 	system("pause");
 	return 0;
 }
